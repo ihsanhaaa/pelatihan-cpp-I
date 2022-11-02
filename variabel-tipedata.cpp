026fb9f53fc5a71ac9fac841914bc3fab406ec5a
Ihsan Haryansyah <64933556+ihsanhaaa@users.noreply.github.com>
2022-11-03 02:02:11 +0700

    Create variabel-tipedata.cpp

---
+++ variabel-tipedata.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	// materi variabel dan penggunaan tipe data
+	
+	string nama = "Budi";
+	int umur = 22;
+	string hobi = "Saya suka belajar bahasa pemrograman";
+	float tinggi_badan = 165.5;
+	
+	cout << "Nama = " << nama << endl;
+	cout << "Umur = " << umur << endl;
+	cout << "Hobi = " << hobi << endl;
+	cout << "Tinggi Badan = " << tinggi_badan << endl;
+	
+	return 0;
+}
