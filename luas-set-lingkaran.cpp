baac6b38935595f3bfabbc1c09ea5591bb8c594d
Ihsan Haryansyah <64933556+ihsanhaaa@users.noreply.github.com>
2022-11-03 02:34:28 +0700

    Update luas-set-lingkaran.cpp

--- luas-set-lingkaran.cpp
+++ luas-set-lingkaran.cpp
@@ -6,12 +6,14 @@ int main() {
 	/*materi penggunaan tipe data dan 
 	operator mencari luas setengah lingkaran */
 	
-	// int adalaah tipe data bilangan bulat
-	// a, b, c adalah nama variabel
+	// int adalah tipe data bilangan bulat
+  	// float adalah tipe data bilangan pecahan
+	// phi, r, l adalah nama variabel
 	float phi = 3.14;
 	int r = 10;
 	int l;
 	
+	// rumus mencari luas setengah lingkaran
 	l = (phi * r * r) / 2;
 	
 	cout << "Hasil perhitungan luas setengah lingkaran = " << l;
