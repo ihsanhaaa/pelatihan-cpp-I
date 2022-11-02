5958f577dc7fe1510b2ba509531da5f91804b1f7
Ihsan Haryansyah <64933556+ihsanhaaa@users.noreply.github.com>
2022-11-03 02:56:36 +0700

    Create kkm.cpp

---
+++ kkm.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	/*materi penggunaan tipe data dan 
+	percabangan atau kondisi */
+	
+	// int adalah tipe data bilangan bulat
+	// nilai dan kkm adalah nama variabel
+	int nilai = 80;
+	int kkm = 75;
+	
+	// melakukan pengecekan
+	if (nilai > kkm) {
+		cout << "Selamat nilai anda diatas target";
+	}
+	
+	return 0;
+}
