be680ef633464998a7e8791e75d37aa44e850535
Ihsan Haryansyah <64933556+ihsanhaaa@users.noreply.github.com>
2022-11-03 02:18:03 +0700

    Create tipedata-operator.cpp

---
+++ tipedata-operator.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	// materi penggunaan tipe data dan operator
+	
+	// int adalaah tipe data bilangan bulat
+	// a, b, c adalah nama variabel
+	int a = 10;
+	int b = 15;
+	int c;
+	
+	c = a + b;
+	
+	cout << a << " + " << b << " = " << c;
+	
+	return 0;
+}
