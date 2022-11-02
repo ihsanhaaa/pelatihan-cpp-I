76fd04215e04c8fe5cbae46f23df1b42e6a08d07
Ihsan Haryansyah <64933556+ihsanhaaa@users.noreply.github.com>
2022-11-03 02:31:49 +0700

    Create luas-set-lingkaran.cpp

---
+++ luas-set-lingkaran.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	/*materi penggunaan tipe data dan 
+	operator mencari luas setengah lingkaran */
+	
+	// int adalaah tipe data bilangan bulat
+	// a, b, c adalah nama variabel
+	float phi = 3.14;
+	int r = 10;
+	int l;
+	
+	l = (phi * r * r) / 2;
+	
+	cout << "Hasil perhitungan luas setengah lingkaran = " << l;
+	
+	return 0;
+}
