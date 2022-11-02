c9b7b5e71df397b26459d26db99991e7dda627e9
Ihsan Haryansyah <64933556+ihsanhaaa@users.noreply.github.com>
2022-11-03 02:24:21 +0700

    Create tipedata-operator-perkalian.cpp

---
+++ tipedata-operator-perkalian.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	// materi penggunaan tipe data dan operator perkalian
+	
+	// int adalaah tipe data bilangan bulat
+	// a, b, c adalah nama variabel
+	float a = 13.5;
+	int b = 15;
+	float c;
+	
+	c = a * b;
+	
+	cout << a << " x " << b << " = " << c;
+	
+	return 0;
+}
