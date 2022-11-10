fdda3a920a0aaa5a3e9e28e5ca87cf599eb35b3c
Ihsan Haryansyah <64933556+ihsanhaaa@users.noreply.github.com>
2022-11-10 09:22:11 +0700

    Create cout.cpp

---
+++ cout.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	
+	cout << "Hello world" << endl;
+	cout << "Ini akan ditampilkan pada layar";
+	cout << "Baris ini tidak ada spasinya" << endl;
+	cout << "Ini adalah baris akhir";
+	
+	return 0;
+}
