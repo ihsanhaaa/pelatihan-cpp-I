40d2b9e45431fd8a076349fb5a4f6c90cab570d4
Ihsan Haryansyah <64933556+ihsanhaaa@users.noreply.github.com>
2022-11-10 09:29:27 +0700

    Create cin.cpp

---
+++ cin.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+    string nama;
+    
+    cout << "Tuliskan nama: ";
+    cin >> nama;
+
+    cout << "Hi " << nama;
+    cout << " Selamat datang di pelatihan C++ dasar" << endl;
+
+    return 0;
+}
